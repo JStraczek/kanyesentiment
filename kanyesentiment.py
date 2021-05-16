49c326f020a549ff77248137b161b7272d1f77bc
unknown <kubastr98@gmail.com>
2021-05-16 18:51:45 +0200

    added sentiment counting

--- kanyesentiment.py
+++ kanyesentiment.py
@@ -1,6 +1,7 @@
 import json
 import requests
 
+
 def get_quotes(how_many):
     quote_list = []
     for i in range(how_many):
@@ -9,7 +10,7 @@ def get_quotes(how_many):
             res = requests.get(url)
             quote = json.loads(res.text)['quote']
             if quote in quote_list:
-                how_many += 1
+                i -= 1
                 continue
             quote_list.append(quote)
         except Exception:
@@ -43,19 +44,31 @@ def solve():
     extreme_quote = quote_list[0]
     extreme_sentiment, extreme_polarity = read_sentiment(quote_list[0])
     has_extreme = False
+    tmp = extreme_polarity
+
+    count_dict = {'positive': 0, 'negative': 0, 'neutral': 0}
 
     print('Kanye quotes with sentiment evalutation: ')
     for i, quote in enumerate(quote_list):
         print(str(i+1) + '.', quote)
         quote_sentiment, quote_polarity = read_sentiment(quote)
-        if abs(quote_polarity) > abs(extreme_polarity):
+        count_dict[quote_sentiment] += 1
+
+        if quote_polarity != tmp:
             has_extreme = True
+        tmp = quote_polarity
+
+        if abs(quote_polarity) > abs(extreme_polarity):
             extreme_polarity = quote_polarity
             extreme_sentiment = quote_sentiment
             extreme_quote = quote
 
         print(
             f'This quote is {quote_sentiment} and it\'s polarity is: {quote_polarity}\n')
+
+    print(f'Positive quotes: {count_dict["positive"]}')
+    print(f'Negative quotes: {count_dict["negative"]}')
+    print(f'Neutral quotes: {count_dict["neutral"]}\n')
 
     if has_extreme:
         print(
